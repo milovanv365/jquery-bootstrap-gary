b7f59fae9e8ec92d2c9df3eedaf552e5fdce4e03
fullstack <fullstack@test.com>
2020-03-16 10:07:48 +0100

    changed html to php, added base structure and home2.php for layout page B

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer class="bg-color1 text-white">
+    <div class="row">
+        <p class="col-sm-6 pa-main">Animal Cancer Trust © 2020</p>
+        <p class="col-sm-6 pa-main text-md-right">Registered Charity Number: 1094779</p>
+    </div>
+</footer>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src="assets/js/main.js"></script>
+
+<script>
+</script>
+</body>
+</html>
---
+++ home2.php
@@ -0,0 +1,49 @@
+<?php require 'base.php'; ?>
+
+<section>
+    <div class="row">
+        <div class="col-md-12 col-lg-8 py-3">
+            <div class="pt-3 px-3">
+                <p class="color1 text-x1 font-weight-bolder">Grid Layout 1</p>
+                <p class="py-2">
+                    Our Aims as a charity are to: <br />
+                    Provide education and information that help owners, the veterinary profession and all who care for our pets understand
+                    more about cancer and what treatments are possible.
+                    Facilitate ethical research and improve our understanding and treatment of cancer.
+                    Work with partners, at home and abroad, to pursue the fight against cancer in our pets.
+                </p>
+                <p class="py-3">
+                    Our goal for the next 5 years is to meet our first aim listed above in order to:
+                    Support owners in their journey dealing with cancer in pets and
+                    Equip pet professionals to help them do the same by providing
+                </p>
+            </div>
+            <div class="row pt-3 px-3">
+                <div class="col-12 col-sm-4 px-0">
+                    <img src="assets/img/animal-cancer-trust-3.jpg" class="w-100"/>
+                </div>
+                <div class="col-12 col-sm-8 pl-4 py-3 py-sm-0">
+                    <p>
+                        Our new website and ACT now to become a CAP (Cancer Alert Practice)
+                        campaign will help us to:
+                        Provide informative, independent educational material to pet owners
+                        Provide a resource for owners looking for oncology services for their pets
+                        Educate and inform veterinary and pet professionals on topics associated
+                        with cancer in pets
+                        Generate sufficient income to maintain activities
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-0 col-lg-2"></div>
+        <div class="row col-md-12 col-lg-2 bg-color2 right-img-box mx-3 mx-sm-0">
+            <ul class="list-unstyled w-100 row px-md-2 px-lg-0 py-2">
+                <li class="col-sm-4 col-lg-12 px-md-2 px-lg-3 py-2"><a href="#"><img src="assets/img/animal-cancer-trust-3.jpg" class="w-100" /></a></li>
+                <li class="col-sm-4 col-lg-12 px-md-2 px-lg-3 py-2"><a href="#"><img src="assets/img/animal-cancer-trust-4.jpg" class="w-100" /></a></li>
+                <li class="col-sm-4 col-lg-12 px-md-2 px-lg-3 py-2"><a href="#"><img src="assets/img/animal-cancer-trust-5.jpg" class="w-100" /></a></li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+<?php require 'footer.php'; ?>
